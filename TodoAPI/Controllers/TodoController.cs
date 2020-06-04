910b32b068d1ac1adea4602cdd40d35222f0d5ea
Tony Sulfaro <tony116523@gmail.com>
2020-06-04 12:13:48 -0400

    Add project files.

---
+++ TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context) {
+            _context = context;
+        }
+
+        // GET: api/<TodoController>
+        [HttpGet]
+        public IEnumerable<string> Get() {
+
+            var item = _context.TodoItems.Find(1);
+
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TodoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/<TodoController>
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+        // PUT api/<TodoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<TodoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoAPI.Models;
+
+namespace TodoAPI.Models {
+    public class TodoContext: Microsoft.EntityFrameworkCore.DbContext {
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
