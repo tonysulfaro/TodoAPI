bb6fcda2748fe6e2748c448d589f411c572f5f88
Tony Sulfaro <tony116523@gmail.com>
2020-06-08 08:40:39 -0400

    now time to add authentication

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -23,6 +23,8 @@ namespace TodoAPI {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddCors();
             services.AddControllers();
 
             // database
@@ -32,6 +34,13 @@ namespace TodoAPI {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
